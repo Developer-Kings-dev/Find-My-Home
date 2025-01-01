4970a4f835823dbd0bc5db835982ca59136520ec
Developer-Kings-Dev <developerkingsdevops@gmail.com>
2025-01-01 10:07:52 -1200

    Initial commit for Find My Home backend

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,23 @@
 // config/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebaseadminsdk.json');
+// const serviceAccount = require('../firebaseadminsdk.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   storageBucket: "gs://accomodation-app-fb0b2.appspot.com"
+// });
+
+// const bucket = admin.storage().bucket();
+// module.exports = { bucket };
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://accomodation-app-fb0b2.appspot.com"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
 });
 
 const bucket = admin.storage().bucket();
-module.exports = { bucket };
+module.exports = bucket;
